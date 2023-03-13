5013c135d94fbfe883b17eb5223bcc6c9fcbda76
kapellaz <rpcsantos192002@hotmail.com>
2023-03-13 18:08:57 +0000

    merda fodida

---
+++ projeto_sd/src/Message/IQueueRemoteInterface.java
@@ -0,0 +1,9 @@
+package Message;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+public interface IQueueRemoteInterface extends Remote {
+    String retira() throws RemoteException;
+    void coloca(String e) throws RemoteException;
+}
